05624ec45bd8b682ff489eeac9eec23e4e8be71e
Edvinas Riepsas <edvinas.riepsas@gmail.com>
2023-09-30 20:32:34 +0300

    Add GET route for all records
    * Add boilerplate for other routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,24 +1,54 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import ExampleSchema from "../schemas/exampleShema";
 import { RequestError } from "../utils/errorHandlers";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_: never, res: Response) => {
   console.log("Hello from home.");
   res.sendStatus(200);
 });
 
-router.post("/test", async (req, res, next) => {
+router.post(
+  "/test",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const test = new ExampleSchema(req.body);
+      await test.save();
+      res.sendStatus(200);
+      return;
+    } catch (err) {
+      next(RequestError.InternalError(err.message));
+      return;
+    }
+  }
+);
+
+router.get("/test", async (_: never, res: Response, next: NextFunction) => {
   try {
-    const test = new ExampleSchema(req.body);
-    await test.save();
-    res.sendStatus(200);
+    const test = await ExampleSchema.find({});
+    console.log(test);
+    res.status(200).json(test);
     return;
   } catch (err) {
     next(RequestError.InternalError(err.message));
     return;
   }
 });
+
+router.get(
+  "/test/:id",
+  async (req: Request, res: Response, next: NextFunction) => {}
+);
+
+router.put(
+  "/test/:id",
+  async (req: Request, res: Response, next: NextFunction) => {}
+);
+
+router.delete(
+  "/test/:id",
+  async (req: Request, res: Response, next: NextFunction) => {}
+);
 
 export default router;
